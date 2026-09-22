bd8859acb6f94e86e52710f5ad715d31191ecc33
Giovanna <giscalzone@gmail.com>
2026-09-22 20:15:47 -0300

    feat: Adicionando as funcionalidades no aplicativo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Funcionalidades from './components/Funcionalidades'
 import Menu from './components/Menu'
 
 const App = () => {
     return (
         <>
             <Menu />
+            <Funcionalidades />
         </>
     )
 }
