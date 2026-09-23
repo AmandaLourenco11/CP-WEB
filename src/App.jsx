bef55b28f9ba13e7d72076f40da4fd2a48b043e7
Amanda <amandalourenco.2512@gmail.com>
2026-09-22 21:26:03 -0300

    feat:Implementação do componente Depoimentos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Sobre  from './components/Sobre'
 import Funcionalidades from './components/Funcionalidades'
 import Menu from './components/Menu'
+import Depoimentos from './components/Depoimentos'
 
 const App = () => {
     return (
         <>
+            <Header />
+            <Hero />
+            <Sobre />
             <Menu />
             <Funcionalidades />
+            <Depoimentos />
+            
         </>
     )
 }
